75dae00ff8680ab81d027d330a63f95d2177f0fc
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-16 15:00:33 +0100

    Update localizationmanager.lua

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -4,6 +4,10 @@ Hooks:Add("LocalizationManagerPostInit", "Leech_Bad", function(loc)
         ["menu_deck22_3_desc"] = "Your maximum health is increased by ##20%##."
     } )
 	loc:add_localized_strings( {
+        menu_deck22_5 = "Stalwart",
+        ["menu_deck22_5_desc"] = "Killing an enemy reduces the cooldown of Leech Ampule by ##1## second(s)."
+    } )
+	loc:add_localized_strings( {
         menu_deck22_9 = "Symbiosis",
         ["menu_deck22_9_desc"] = "Your maximum health is increased by ##60%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during Payday is increased by ##10%##."
    } )
