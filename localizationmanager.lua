1b717d4ef591c7b682b550e3895ea132737cbfa2
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-16 14:42:54 +0100

    Update localizationmanager.lua

--- localizationmanager.lua
+++ localizationmanager.lua
@@ -4,7 +4,7 @@ Hooks:Add("LocalizationManagerPostInit", "Leech_Bad", function(loc)
         ["menu_deck22_3_desc"] = "Your maximum health is increased by ##20%##."
     } )
 	loc:add_localized_strings( {
-        menu_deck22_9 = "Unseen Strike",
+        menu_deck22_9 = "Built Different",
         ["menu_deck22_9_desc"] = "Your maximum health is increased by ##60%##.\n\nDeck Completion Bonus: Your chance of getting a higher quality item during Payday is increased by ##10%##."
    } )
 end )
