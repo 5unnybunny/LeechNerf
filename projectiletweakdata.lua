1c47c0175550db58ae762d279168d474cc6f4737
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-01-27 00:06:27 +0100

    Update projectiletweakdata.lua

--- projectiletweakdata.lua
+++ projectiletweakdata.lua
@@ -1,3 +1,3 @@
-Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "HCSP_crimespreetweakdata", function(self)
+Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "Leech_Nerf_Projectile_Duration", function(self)
     self.projectiles.copr_ability.base_cooldown = 40
 end)
