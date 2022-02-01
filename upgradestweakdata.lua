eac82d626614266a08ed818d4fbb296cd7c794a1
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-01 15:52:16 +0100

    Update upgradestweakdata.lua

--- upgradestweakdata.lua
+++ upgradestweakdata.lua
@@ -1,6 +1,7 @@
 Hooks:PostHook(UpgradesTweakData, "init", "Leech_Nerf_UpgradesTweakData", function(self)
     --cooldown description
-    self.specialization_descs[22][1].multiperk6 = "40"
+    self.specialization_descs[22][1].multiperk6 = 40
+    self.specialization_descs[22][1].multiperk3 = 6
     
     --required kills increased to 5
     self.values.player.copr_kill_life_leech = {
