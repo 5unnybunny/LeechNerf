802d8ea61e51c0889eed2767f861d5f84fd1e832
Bunny_Cunny <97416657+bunnycunny@users.noreply.github.com>
2022-02-07 04:47:12 +0100

    Update upgradestweakdata.lua

--- upgradestweakdata.lua
+++ upgradestweakdata.lua
@@ -1,7 +1,7 @@
 Hooks:PostHook(UpgradesTweakData, "init", "Leech_Nerf_UpgradesTweakData", function(self)
 	--cooldown description
 	self.specialization_descs[22][1].multiperk3 = "5"
-	self.specialization_descs[22][1].multiperk6 = "40"
+	self.specialization_descs[22][1].multiperk6 = "60"
 		
 	--nerf cooldown
 	self.copr_ability_cooldown = 60
